aecdc090ef6ff3db178ca43ed5a4f8d5137cd827
Dennis LLopis <dllopis16@gmail.com>
2024-02-14 22:14:02 -0500

    testing some changes to path structure

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import PrepareQuiz from "./components/PrepareQuiz";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cryptography-networking-study-app/" element={<Home />} />
-        <Route path="cryptography-networking-study-app/Chapter/1/flashcards" element={<KeyTermsCarousel chapter="1" />} />
-        <Route path="cryptography-networking-study-app/Chapter/1" element={<KeyTerms chapter="1" />} />
-        <Route path="cryptography-networking-study-app/Chapter/1/multiplechoicequiz" element={<PrepareQuiz chapter="1" />} />
+        <Route path="/cryptography-networking-study-app/Chapter/1/flashcards" element={<KeyTermsCarousel chapter="1" />} />
+        <Route path="/cryptography-networking-study-app/Chapter/1" element={<KeyTerms chapter="1" />} />
+        <Route path="/cryptography-networking-study-app/Chapter/1/multiplechoicequiz" element={<PrepareQuiz chapter="1" />} />
       </Routes>
     </Router>
   );
